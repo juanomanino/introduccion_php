679875c61bbd5310b84cddb8dc24cab100b19ade
juanomanino <juannino@colegioguanenta.edu.co>
2023-07-17 17:27:36 -0500

    Comentarios en PHP

---
+++ 1_comentarios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios en PHP</title>
+</head>
+<body>
+    <h1>Comentarios en PHP</h1>
+    <!--Fomas de incluir codigo php-->
+    <?php
+        //Comentario de una línea
+        /* Esto es un comentario 
+        de varias lineas*/
+    ?>
+
+
+<!-- 
+    <%
+        codigo php
+    %>
+
+    <script language="php"></script> -->
+
+
+</body>
+</html>
